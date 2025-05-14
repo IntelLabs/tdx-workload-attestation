b9fff3c5060c90f8ac86d7e14051a51e796480e0
Marcela Melara <marcela.melara@intel.com>
2025-05-14 16:48:45 -0700

    Remove asserts in device
    Signed-off-by: Marcela Melara <marcela.melara@intel.com>

--- src/tdx/linux/device.rs
+++ src/tdx/linux/device.rs
@@ -74,10 +74,14 @@ impl TdxDeviceKvmV15 {
     /// Creates a new instance of `TdxDeviceKvmV15`, and ensures that the TDX
     /// device node is available before creating the instance.
     pub fn new() -> TdxDeviceKvmV15 {
-        assert!(Self::is_available().expect("Intel TDX KVM device node supported"));
-
-        TdxDeviceKvmV15 {
-            device_path: TDX15_DEV_PATH.to_string(),
+        match Self::is_available() {
+            Ok(true) => TdxDeviceKvmV15 {
+                device_path: TDX15_DEV_PATH.to_string(),
+            },
+            // return an empty device path, if TDX isn't available or there was an error
+            _ => TdxDeviceKvmV15 {
+                device_path: "".to_string(),
+            },
         }
     }
 
@@ -103,7 +107,13 @@ impl TdxDeviceKvmV15 {
     /// Retrieves the raw TD report (Quote/Signed Attestation Report) from the
     /// TDX device by using an ioctl system call to interact with the device.
     pub fn get_tdreport_raw(&self, &req: &[u8; 1088]) -> Result<[u8; 1088]> {
-        assert!(TdxDeviceKvmV15::is_available().expect("Intel TDX KVM device node supported"));
+        // Before we do anything, check if the device_path is empty.
+        // If it is, TDX isn't supported, throw an error
+        if self.device_path.is_empty() {
+            return Err(Error::NotSupported(
+                "TDX 1.5 KVM device is not supported".to_string(),
+            ));
+        }
 
         // 1. Get device file descriptor: must open in RW mode
         let tdx_dev = fs::File::options()
@@ -116,8 +126,6 @@ impl TdxDeviceKvmV15 {
                     self.device_path, e
                 ))
             })?;
-
-        println!("Found TDX device: {:?}", tdx_dev);
 
         let mut resp = req;
 
@@ -147,20 +155,22 @@ mod tests {
     fn test_is_available() -> Result<()> {
         match TdxDeviceKvmV15::is_available() {
             Ok(true) => {
-                println!("KVM TDX 1.5 device available");
+                let path = Path::new(TDX15_DEV_PATH);
+                assert!(fs::exists(path).expect("TDX 1.5 KVM device should be available"));
                 Ok(())
             }
             Ok(false) => {
-                println!("Path was a broken symlink");
+                println!("TDX device is a broken symlink, which is not supported");
                 Ok(())
             }
-            Err(e) => {
-                println!(
-                    "An error occurred trying to read the KVM TDX 1.5 device: {}",
-                    e
-                );
-                Ok(())
-            }
+            Err(e) => match e {
+                Error::NotSupported(_) => {
+                    println!("{}", e);
+                    Ok(())
+                }
+                // Any other error type is unexpected
+                _ => Err(e),
+            },
         }
     }
 
@@ -175,10 +185,18 @@ mod tests {
                 assert!(report != [0; 1088]);
                 Ok(())
             }
-            Err(e) => {
-                println!("An error occurred trying to quote the TD: {}", e);
-                Ok(())
-            }
+            Err(e) => match e {
+                Error::NotSupported(_) => {
+                    println!("{}", e);
+                    Ok(())
+                }
+                // Any other error type is unexpected
+                Error::QuoteError(_) => {
+                    println!("{}", e);
+                    Ok(())
+                }
+                _ => Err(e),
+            },
         }
     }
 }
